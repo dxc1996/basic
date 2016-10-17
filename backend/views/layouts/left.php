d5387c16c7be23d904685366c6f6ba36efab51ac
daidai <13022779332@163.com>
2016-10-17 11:03:30 +0800

    dd

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -63,6 +63,34 @@
                 ],
             ]
         ) ?>
+        <ul class="sidebar-menu">
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-gears"></i> <span>权限控制</span>
+                    <i class="fa fa-angle-left pull-right"></i>
+                </a>
+                <ul class="treeview-menu">
+                    <li class="treeview">
+                        <a href="/admin">管理员--</a>
+                        <ul class="treeview-menu">
+                            <li><a href="/user"><i class="fa fa-circle-o"></i> 后台用户</a></li>
+                            <li class="treeview">
+                                <a href="/admin/role">
+                                    <i class="fa fa-circle-o"></i> 权限 <i class="fa fa-angle-left pull-right"></i>
+                                </a>
+                                <ul class="treeview-menu">
+                                    <li><a href="/admin/route"><i class="fa fa-circle-o"></i> 路由</a></li>
+                                    <li><a href="/admin/permission"><i class="fa fa-circle-o"></i> 权限</a></li>
+                                    <li><a href="/admin/role"><i class="fa fa-circle-o"></i> 角色</a></li>
+                                    <li><a href="/admin/assignment"><i class="fa fa-circle-o"></i> 分配</a></li>
+                                    <li><a href="/admin/menu"><i class="fa fa-circle-o"></i> 菜单</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+        </ul>
 
     </section>
 
