25c6f3691c638592623c9402037e61b80c9bba30
daidai <13022779332@163.com>
2016-10-17 16:21:35 +0800

    dd

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -1,3 +1,7 @@
+<?php
+    use yii\bootstrap\Nav;
+    use mdm\admin\components\MenuHelper;
+?>
 <aside class="main-sidebar">
 
     <section class="sidebar">
@@ -91,7 +95,33 @@
                 </ul>
             </li>
         </ul>
-
+        <?php
+        $callback = function($menu){
+            $data = json_decode($menu['data'], true);
+            $items = $menu['children'];
+            $return = [
+                'label' => $menu['name'],
+                'url' => [$menu['route']],
+            ];
+            //处理我们的配置
+            if ($data) {
+                //visible
+                isset($data['visible']) && $return['visible'] = $data['visible'];
+                //icon
+                isset($data['icon']) && $data['icon'] && $return['icon'] = $data['icon'];
+                //other attribute e.g. class...
+                $return['options'] = $data;
+            }
+            //没配置图标的显示默认图标
+            (!isset($return['icon']) || !$return['icon']) && $return['icon'] = 'fa fa-circle-o';
+            $items && $return['items'] = $items;
+            return $return;
+        };
+        //这里我们对一开始写的菜单menu进行了优化
+        echo dmstr\widgets\Menu::widget( [
+            'options' => ['class' => 'sidebar-menu'],
+            'items' => MenuHelper::getAssignedMenu(Yii::$app->user->id, null, $callback),
+        ] ); ?>
     </section>
 
 </aside>
