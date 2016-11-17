2d23ae10dfba3fd7939354134ec440cc57771308
daidai <13022779332@163.com>
2016-11-17 13:28:22 +0800

    admin views

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -2,21 +2,21 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+$this->title = '后台管理';
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <h1>Blogs</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p class="lead">博客后台管理</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <!--<p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>-->
     </div>
 
     <div class="body-content">
 
-        <div class="row">
+        <!--<div class="row">
             <div class="col-lg-4">
                 <h2>Heading</h2>
 
@@ -47,7 +47,7 @@ $this->title = 'My Yii Application';
 
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
             </div>
-        </div>
+        </div>-->
 
     </div>
 </div>
